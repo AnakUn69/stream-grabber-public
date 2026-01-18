dd82644aff7b494f50cf685ec6b1dec0f989446d
AnakUn69 <7vagnma@gmail.com>
2026-01-18 01:13:10 +0100

    bigger w2g icon

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-const W2G_ICON = `<img src="icons/w2g.svg" style="width: 50px; height: auto; display: block; margin: auto;" alt="W2G">`;
+const W2G_ICON = `<img src="icons/w2g.svg" style="width: 100px; height: auto; display: block; margin: auto;" alt="W2G">`;
 const COPY_ICON = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="btn-icon"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"/><path d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2"/><path d="M16 4h2a2 2 0 0 1 2 2v4"/><path d="M21 14H11"/><path d="m15 10-4 4 4 4"/></svg>`;
 const content = document.getElementById("content");
 const settingsDiv = document.getElementById("settings");
